e69ff88e64da190a22b5653277af37e3271aa2e3
Syed Zaidi <syed.zaidi.hussain@gmail.com>
2023-04-22 12:34:33 -0400

    forgot to change position of the entities and migrations

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
-require('dotenv');
-const myDataSource = new typeorm_1.DataSource({
+require('dotenv').config({ path: './.env' });
+console.log('Current working directory:', process.cwd());
+console.log('Environment variables:', process.env);
+console.log({
     type: 'postgres',
     host: process.env.HOST,
     username: process.env.USERNAME,
@@ -11,6 +13,19 @@ const myDataSource = new typeorm_1.DataSource({
     database: process.env.DBNAME,
     entities: ["./src/entities/*.ts"],
     migrations: ["./src/migrations/*.ts"],
+    logging: true,
+    synchronize: true,
+});
+const myDataSource = new typeorm_1.DataSource({
+    type: 'postgres',
+    host: process.env.HOST,
+    username: process.env.USERNAME,
+    ssl: true,
+    password: process.env.PASS,
+    port: 5432,
+    database: process.env.DBNAME,
+    entities: ["./dist/entities/*.js"],
+    migrations: ["./dist/migrations/*.js"],
     logging: true,
     synchronize: true,
 });
