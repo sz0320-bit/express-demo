fa054b99a2624c36224589f3f14dfd3a63e128fb
Syed Zaidi <syed.zaidi.hussain@gmail.com>
2023-04-21 23:06:31 -0400

    retrying where i place the env

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,11 +1,28 @@
 import { DataSource } from "typeorm"
-require('dotenv').config({ path: '../.env' });
 
+require('dotenv').config({ path: './.env' });
+console.log('Current working directory:', process.cwd());
+console.log('Environment variables:', process.env);
+
+
+console.log({
+   type: 'postgres',
+   host: process.env.HOST,
+   username: process.env.USERNAME,
+   password: process.env.PASS,
+   port: 5432,
+   database: process.env.DBNAME,
+   entities: ["./src/entities/*.ts"],
+   migrations: ["./src/migrations/*.ts"],
+   logging: true,
+   synchronize: true,
+})
 
  const myDataSource = new DataSource({
     type: 'postgres',
     host: process.env.HOST,
     username: process.env.USERNAME,
+    ssl: true,
     password: process.env.PASS,
     port: 5432,
     database: process.env.DBNAME,
